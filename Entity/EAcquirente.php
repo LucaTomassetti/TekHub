f6e68f7938bf0daf31990c77745681d682c4b125
LucaTomassetti <luca.tomassetti02@gmail.com>
2024-04-12 12:13:56 +0200

    Creazione classe EAcquirente.php

---
+++ Entity/EAcquirente.php
@@ -0,0 +1,16 @@
+<?php
+class EAcquirente{
+    private String $nome;
+    private String $cognome;
+    private String $id;
+    private String $username;
+    private String $password;
+    private String $email;
+    private String $cellulare;
+
+    public function __construct($nome,$cognome,$id,$username,$password,$email,$cellulare){
+        
+    }
+}
+
+?>
