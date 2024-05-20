708ce48c60f06331b6aa2d2360170590e521f757
LucaTomassetti <luca.tomassetti02@gmail.com>
2024-05-20 16:22:13 +0200

    routing functions for controllers created

--- main.php
+++ main.php
@@ -2,6 +2,29 @@
 require_once __DIR__ .'/src/Utility/autoload.inc.php';
 require_once __DIR__ .'/config/bootstrap.php';
 
+function show($arg){
+    echo "<pre>";
+    print_r($arg);
+    echo "</pre>";
+}
+function splitURL(){
+    $URL = $_GET['url'];
+    $URL = explode('/', $URL);
+    return $URL;
+}
+function loadController(){
+    $URL = splitURL();
+    $file = "./src/Controller/C".ucfirst($URL[0]).".php";
+    if(file_exists($file)){
+        require $file;
+    }
+    else{
+        $file = "./src/Controller/_404.php";
+        require $file;
+    }
+}
+show(splitURL());
+loadController();
 
 $a1 = new EAcquirente(NULL,'boh','boh','boh','boh','boh@boh.com',3891237564);
 
