251426b6ccd2326b200df381a81dacab043bb456
LucaTomassetti <luca.tomassetti02@gmail.com>
2024-06-03 20:02:36 +0200

    comment deleted

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
-//require_once __DIR__ .'/src/Utility/autoload.inc.php';
 require_once __DIR__ .'/config/bootstrap.php';
 require_once __DIR__ . "/config/StartSmarty.php";
-/**
- * function show($arg){
-    echo "<pre>";
-    print_r($arg);
-    echo "</pre>";
-    }
- */
 
  $fc = new CFrontController();
  $fc->run();
