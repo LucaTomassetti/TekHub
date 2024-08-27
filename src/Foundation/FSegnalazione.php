a4685a58cf31b285466fa3835f8ef74a42553d8e
FrancescoPioDF <francesco.pio.difelice@gmail.com>
2024-08-27 18:09:57 +0200

    aggiunte FSegnalazione e FRecensione

---
+++ src/Foundation/FSegnalazione.php
@@ -0,0 +1,18 @@
+<?php
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Query\Parameter;
+
+class FSegnalazione extends EntityRepository {
+
+    public function findSegnalazione($id){
+        $dql= "SELECT segnalazione FROM ESegnalazione segnalazione WHERE segnalazione.id_segnalazione=?1";
+        $query=getEntityManager()->createQuery();
+        $query->SetParameter(1,$id);
+        $query->setMaxResults(1);
+        return $query->getResult();   
+    }
+
+
+
+}
